e332344552d7475afb8edc0d56db47dcbd96896e
Rich Neswold <neswold@fnal.gov>
2023-12-13 10:38:27 -0600

    :wrench: definitely use DPM09

--- src/g_rpc/dpm/mod.rs
+++ src/g_rpc/dpm/mod.rs
@@ -10,7 +10,7 @@ pub mod proto {
 pub async fn acquire_devices(
     session_id: &str, devices: Vec<String>,
 ) -> Result<tonic::Response<tonic::Streaming<proto::Reading>>, tonic::Status> {
-    match DpmClient::connect("http://dce46.fnal.gov:50051/").await {
+    match DpmClient::connect("http://dce09.fnal.gov:50051/").await {
         Ok(mut client) => {
             let req = AcquisitionList {
                 session_id: session_id.to_owned(),
@@ -31,7 +31,7 @@ pub async fn set_device(
 ) -> Result<i32, tonic::Status> {
     info!("setting device {} to {:?}", &device, &value);
 
-    match DpmClient::connect("http://dce46.fnal.gov:50051/").await {
+    match DpmClient::connect("http://dce09.fnal.gov:50051/").await {
         Ok(mut client) => {
             // Build the setting request. This function only sets one
             // device, so the request only has a 1-element array
