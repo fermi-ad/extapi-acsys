d5933890787607afa60fa40300ae4a3ae5f30648
Rich Neswold <neswold@fnal.gov>
2024-04-30 10:55:13 -0500

    :wrench: use new, "permanent" IP address

--- src/g_rpc/devdb/mod.rs
+++ src/g_rpc/devdb/mod.rs
@@ -7,7 +7,7 @@ pub mod proto {
 pub async fn get_device_info(
     device: &[String],
 ) -> Result<tonic::Response<proto::DeviceInfoReply>, tonic::Status> {
-    match DevDbClient::connect("http://10.200.24.120:6802/").await {
+    match DevDbClient::connect("http://10.200.24.105:6802/").await {
         Ok(mut client) => {
             let req = proto::DeviceList {
                 device: device.to_vec(),
