3219da92cf673c0e7aa2f20d9bd5baa0c25e0f26
Rich Neswold <neswold@fnal.gov>
2024-08-14 15:34:38 -0500

    :construction: use `axum` crate instead of `warp`
    This is still a work-in-progress. Right now it compiles completely, but it
    hasn't been tested at all and it still needs as few features:
    
    - Support TLS (right now it only supports HTTP)
    - Add CORS support

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,6 +1,7 @@
 use async_graphql::*;
-use std::{convert::Infallible, path::Path};
-use warp::{Filter, Rejection};
+use async_graphql_axum::{GraphQL, GraphQLSubscription};
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
 
 mod acsys;
 mod bbm;
@@ -32,27 +33,37 @@ struct Subscription(
     xform::XFormSubscriptions,
 );
 
-// Final schema type.
+//const AUTH_HEADER: &str = "acsys-auth-jwt";
 
-type MySchema = Schema<Query, Mutation, Subscription>;
+// Starts the web server that receives GraphQL queries. The
+// configuration of the server is pulled together by obtaining
+// configuration information from the submodules. All accesses are
+// wrapped with CORS support from the `warp` crate.
 
-const AUTH_HEADER: &str = "acsys-auth-jwt";
+pub async fn start_service() {
+    //let filter = filter("acsys").with(
+    //    warp::cors()
+    //        .allow_any_origin()
+    //        .allow_headers(vec![
+    //            AUTH_HEADER,
+    //            "content-type",
+    //            "Access-Control-Allow-Origin",
+    //            "Sec-WebSocket-Protocol",
+    //        ])
+    //        .allow_methods(vec!["OPTIONS", "GET", "POST"]),
+    //);
 
-fn auth_filter(
-) -> impl Filter<Extract = (Option<String>,), Error = Rejection> + Copy {
-    warp::header::optional::<String>(AUTH_HEADER)
-}
+    //warp::serve(filter)
+    //    .tls()
+    //    .cert_path(Path::new("/etc/ssl/private/acsys-proxy.fnal.gov/cert.pem"))
+    //    .key_path(Path::new("/etc/ssl/private/acsys-proxy.fnal.gov/key.pem"))
+    //    .run(([0, 0, 0, 0], 8000))
+    //    .await;
 
-// Returns a Warp Filter that organizes the DPM portion of the web
-// site. The base path is passed in and this function adds filters to
-// recognize and provide GraphQL request support.
+    const ENDPOINT: &str = "/acsys";
 
-fn filter(
-    path: &str,
-) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone + '_
-{
-    // Create the schema object which is used to reply to GraphQL
-    // queries and subscriptions.
+    // Build the GraphQL schema. Also, define the GraphQL interface
+    // (DeviceProperty) that we use in the schema.
 
     let schema = Schema::build(
         Query::default(),
@@ -62,58 +73,28 @@ fn filter(
     .register_output_type::<devdb::types::DeviceProperty>()
     .finish();
 
-    // Build the query portion. This Warp Filter recognizes GraphQL
-    // query and mutation requests.
+    // Create a handler that provides a GraphQL editor so people don't
+    // have to install their own.
 
-    let graphql_query = async_graphql_warp::graphql(schema.clone())
-        .and(auth_filter())
-        .and_then(
-            |(schema, request): (MySchema, async_graphql::Request),
-             _hdr: Option<String>| async move {
-                let resp = schema.execute(request).await;
-
-                Ok::<_, Infallible>(async_graphql_warp::GraphQLResponse::from(
-                    resp,
-                ))
-            },
-        )
-        .with(warp::log("query"));
-
-    // Build the subscription portion. This Warp Filter recognizes
-    // GraphQL subscription requests, which require upgrading the
-    // connection to a WebSocket. This is handled by the library.
-
-    let graphql_sub = async_graphql_warp::graphql_subscription(schema)
-        .with(warp::log("subs"));
-
-    // Build the sub-site. Look, first, for the leading path and then
-    // look for any of the above services.
-
-    warp::path(path).and(graphql_query.or(graphql_sub))
-}
-
-// Starts the web server that receives GraphQL queries. The
-// configuration of the server is pulled together by obtaining
-// configuration information from the submodules. All accesses are
-// wrapped with CORS support from the `warp` crate.
-
-pub async fn start_service() {
-    let filter = filter("acsys").with(
-        warp::cors()
-            .allow_any_origin()
-            .allow_headers(vec![
-                AUTH_HEADER,
-                "content-type",
-                "Access-Control-Allow-Origin",
-                "Sec-WebSocket-Protocol",
-            ])
-            .allow_methods(vec!["OPTIONS", "GET", "POST"]),
+    let graphiql = axum::response::Html(
+        async_graphql::http::GraphiQLSource::build()
+            .endpoint(ENDPOINT)
+            .subscription_endpoint(ENDPOINT)
+            .finish(),
     );
 
-    warp::serve(filter)
-        .tls()
-        .cert_path(Path::new("/etc/ssl/private/acsys-proxy.fnal.gov/cert.pem"))
-        .key_path(Path::new("/etc/ssl/private/acsys-proxy.fnal.gov/key.pem"))
-        .run(([0, 0, 0, 0], 8000))
-        .await;
+    // Build up the routes for the site. We're using the same path
+    // because each of the three services uses a different method
+    // (GET, POST, and WS.)
+
+    let app = Router::new()
+        .route(ENDPOINT, get(graphiql))
+        .route_service(ENDPOINT, GraphQL::new(schema.clone()))
+        .route_service(ENDPOINT, GraphQLSubscription::new(schema));
+
+    // Start the server on port 8000!
+
+    axum::serve(TcpListener::bind("0.0.0.0:8000").await.unwrap(), app)
+        .await
+        .unwrap();
 }
