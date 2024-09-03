4b74052f3d08ec2e2658ca6092a66c5d99a95b9e
Rich Neswold <neswold@fnal.gov>
2024-09-03 13:23:25 -0500

    :recycle: rename some types
    These type name are more consistent with the way the API refers to them.

--- src/graphql/bbm/mod.rs
+++ src/graphql/bbm/mod.rs
@@ -5,15 +5,23 @@ use async_graphql::*;
 pub mod types;
 
 #[derive(Default)]
-pub struct BBMQueries;
+pub struct BbmQueries;
 
 // Define the schema's query entry points. Any methods defined in this
 // section will appear in the schema.
 
+#[doc = "Fermilab tracks the amount of beam transferred through various beamlines. There is a limit that can be transmitted in order to control the amount of radiation that is generated. These queries return information related to the Beam Budget monitoring systems."]
 #[Object]
-impl BBMQueries {
+impl BbmQueries {
     #[doc = "Retrieves beam budget information."]
-    async fn get_beam_budget_info(&self) -> types::BeamBudgetData {
-        types::BeamBudgetData {}
+    async fn bbm_budget_info(&self) -> types::BbmInfo {
+        types::BbmInfo {}
+    }
+
+    #[doc = "Returns device configuration for a specified beamline."]
+    async fn bbm_beamline_config(
+        &self, _beamline: types::Beamline,
+    ) -> Vec<types::BbmDeviceCfg> {
+        vec![]
     }
 }
--- src/graphql/bbm/types.rs
+++ src/graphql/bbm/types.rs
@@ -1,5 +1,4 @@
 use async_graphql::{Enum, Object, SimpleObject};
-use chrono::{DateTime, Utc};
 
 #[doc = "Specifies a beamline."]
 #[derive(Enum, Clone, Copy, PartialEq, Eq)]
@@ -13,53 +12,59 @@ pub enum Beamline {
     Switchyard,
 }
 
-#[doc = "Holds a set of beam budget data. Based on the query, this will hold one or more sets of data."]
-pub struct BeamBudgetData;
+pub struct BbmInfo;
 
+#[doc = "Holds a set of beam budget data. Based on the query, this will hold one or more sets of data."]
 #[Object]
-impl BeamBudgetData {
+impl BbmInfo {
     #[doc = "Contains all the information requested in the query."]
-    pub async fn info(
+    pub async fn data(
         &self,
         #[graphql(
             desc = "If `null`, beam budget information for all beamlines will be returned. If not `null`, it must be a list of machines. The contents will contain information only for the specified machines."
         )]
         _which: Option<Vec<Beamline>>,
-    ) -> Vec<BudgetInfo> {
+    ) -> Vec<BbmData> {
         vec![]
     }
 }
 
-#[doc = "Holds budget information for a machine."]
-pub struct BudgetInfo {
+pub struct BbmData {
     beamline: Beamline,
 }
 
+#[doc = "Holds budget information for a beamline. The query needs to specify the starting time and the number of 5-minute integrations to include in the result."]
 #[Object]
-impl BudgetInfo {
-    #[doc = "Indicates which machine the data is associated."]
+impl BbmData {
+    #[doc = "Indicates which beamline the data is associated."]
     async fn beamline(&self) -> Beamline {
         self.beamline
     }
 
-    #[doc = "Contains the history of the  associated machine."]
+    #[doc = "Contains the history of the associated machine. The array returned by this query will always have `nBins` entries. If there isn't data in a 5-minute window, that entry will be `null`. This helps the application differentiate between a zero reading and a lack of data (in case it wants to display the error.)"]
     async fn history(
         &self,
         #[graphql(
-            desc = "Specifies which run's budget to return. If `null`, the latest run's totals are returned."
+            desc = "Specifies the start time of the data. The time is given in seconds since the Unix epoch (UTC)."
         )]
-        _run: Option<i32>,
-    ) -> Vec<BeamHistory> {
+        _start_time: i32,
+        #[graphql(
+            desc = "Indicates how many 5-minute integration samples to include."
+        )]
+        _n_bins: i32,
+    ) -> Vec<Option<f64>> {
         vec![]
     }
 }
 
+#[doc = "Describes the configuration for a device that returns integrated beam infomation."]
 #[derive(SimpleObject)]
-pub struct BeamHistory {
-    #[doc = "The sample time."]
-    pub timestamp: DateTime<Utc>,
-    #[doc = "The total measured beam up to the sample time."]
-    pub total: f64,
-    #[doc = "The budgeted beam up to the sample time."]
-    pub budget: f64,
+pub struct BbmDeviceCfg {
+    #[doc = "The name of the device."]
+    pub name: String,
+    #[doc = "The clock event on which the device should be read."]
+    pub event: u8,
+    #[doc = "The delay from the clock event (in case there's no clock event on the exact moment. Usually this parameter is 0."]
+    #[graphql(default = 0)]
+    pub delay: usize,
 }
