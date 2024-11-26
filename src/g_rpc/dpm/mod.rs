f3645ab7cc06c7745af20625067e3329b7eb6eda
Rich Neswold <neswold@fnal.gov>
2024-11-26 16:03:48 -0600

    :wrench: point to DPM09

--- src/g_rpc/dpm/mod.rs
+++ src/g_rpc/dpm/mod.rs
@@ -10,7 +10,7 @@ pub mod proto {
 pub async fn acquire_devices(
     session_id: &str, devices: Vec<String>,
 ) -> Result<tonic::Response<tonic::Streaming<proto::Reading>>, tonic::Status> {
-    match DpmClient::connect("http://dce08.fnal.gov:50051/").await {
+    match DpmClient::connect("http://dce09.fnal.gov:50051/").await {
         Ok(mut client) => {
             let req = AcquisitionList {
                 session_id: session_id.to_owned(),
