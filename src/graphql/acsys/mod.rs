c2a3bb1790a695beb3e04fb6e46ccf2af8c34977
Rich Neswold <neswold@fnal.gov>
2024-12-13 14:01:33 -0600

    :sparkles: add base support for plots

--- src/graphql/acsys/mod.rs
+++ src/graphql/acsys/mod.rs
@@ -2,6 +2,7 @@ use crate::g_rpc::dpm;
 
 use async_graphql::*;
 use chrono::{DateTime, Utc};
+use futures::future;
 use futures_util::{stream, Stream, StreamExt};
 use std::pin::Pin;
 use tokio::time::Instant;
@@ -27,23 +28,38 @@ fn mk_xlater(
         + Send
         + Sync,
 > {
-    Box::new(move |e: Result<dpm::proto::Reading, Status>| {
-        let e = e.unwrap();
-
-        if let Some(data) = e.data {
+    Box::new(move |e: Result<dpm::proto::Reading, Status>| match e {
+        Ok(e) => {
+            if let Some(data) = e.data {
+                global::DataReply {
+                    ref_id: e.index as i32,
+                    cycle: 1,
+                    data: global::DataInfo {
+                        timestamp: std::time::SystemTime::now().into(),
+                        result: data.into(),
+                        di: 0,
+                        name: names[e.index as usize].clone(),
+                    },
+                }
+            } else {
+                warn!("returned data: {:?}", &e.data);
+                unreachable!()
+            }
+        }
+        Err(e) => {
+            warn!("channel error: {}", &e);
             global::DataReply {
-                ref_id: e.index as i32,
+                ref_id: 0,
                 cycle: 1,
                 data: global::DataInfo {
                     timestamp: std::time::SystemTime::now().into(),
-                    result: data.into(),
+                    result: global::DataType::StatusReply(
+                        global::StatusReply { status: -1 },
+                    ),
                     di: 0,
-                    name: names[e.index as usize].clone(),
+                    name: "".into(),
                 },
             }
-        } else {
-            warn!("returned data: {:?}", &e.data);
-            unreachable!()
         }
     })
 }
@@ -140,6 +156,73 @@ fn strip_event(drf: &str) -> &str {
     &drf[0..drf.find('@').unwrap_or_else(|| drf.len())]
 }
 
+const NULL_WAVEFORM: &str = "Z:CACHE@N";
+const CONST_WAVEFORM: &str = "API TEST CONST";
+const RAMP_WAVEFORM: &str = "API TEST RAMP";
+const PARABOLA_WAVEFORM: &str = "API TEST PARABOLA";
+const SINE_WAVEFORM: &str = "API TEST SINE";
+
+// Adds a periodic event to a device name to create a DRF specification. The
+// delay indicates the number of milliseconds. If the delay is None, then
+// the delay is 1 second.
+
+fn add_periodic(delay: Option<usize>) -> impl Fn(&str) -> String {
+    move |device| match device {
+        CONST_WAVEFORM | RAMP_WAVEFORM | PARABOLA_WAVEFORM | SINE_WAVEFORM => {
+            NULL_WAVEFORM.into()
+        }
+        _ => format!("{device}@p,{}", delay.unwrap_or(1000)),
+    }
+}
+
+fn stuff_fake_data(
+    r: &mut dyn Iterator<Item = usize>, drfs: &[String],
+    chans: &mut [types::PlotChannelData],
+) {
+    for (idx, chan) in chans.iter_mut().enumerate() {
+        match drfs[idx].as_str() {
+            CONST_WAVEFORM => chan.channel_data = const_data(r, 5.0),
+            RAMP_WAVEFORM => chan.channel_data = ramp_data(r),
+            PARABOLA_WAVEFORM => chan.channel_data = parabola_data(r),
+            SINE_WAVEFORM => chan.channel_data = sine_data(r),
+            _ => (),
+        }
+    }
+}
+
+fn to_plot_data(
+    len: usize, window_size: &Option<usize>, data: &global::DataInfo,
+) -> (i16, Vec<types::PlotDataPoint>) {
+    let step = window_size
+        .filter(|v| *v > 0 && *v <= len)
+        .map(|v| (len + v - 1) / v)
+        .unwrap_or(1);
+
+    match &data.result {
+        global::DataType::Scalar(y) => (
+            0,
+            vec![types::PlotDataPoint {
+                x: 0.0,
+                y: y.scalar_value,
+            }],
+        ),
+        global::DataType::ScalarArray(a) => (
+            0,
+            a.scalar_array_value
+                .iter()
+                .enumerate()
+                .step_by(step)
+                .map(|(idx, y)| types::PlotDataPoint {
+                    x: idx as f64,
+                    y: *y,
+                })
+                .collect(),
+        ),
+        global::DataType::StatusReply(v) => (v.status, vec![]),
+        _ => (-1, vec![]),
+    }
+}
+
 type DataStream = Pin<Box<dyn Stream<Item = global::DataReply> + Send>>;
 type PlotStream = Pin<Box<dyn Stream<Item = types::PlotReplyData> + Send>>;
 
@@ -199,7 +282,7 @@ impl<'ctx> ACSysSubscriptions {
 
     #[doc = ""]
     async fn start_plot(
-        &self,
+        &self, ctxt: &Context<'ctx>,
         #[graphql(
             desc = "List of DRF strings that indicate the devices and return \
 		    rates in which the client is interested."
@@ -229,51 +312,74 @@ impl<'ctx> ACSysSubscriptions {
         )]
         x_max: Option<usize>,
     ) -> PlotStream {
+        info!("incoming plot with delay {:?}", &update_delay);
+
+        // Add the periodic rate to each of the device names after stripping
+        // any event specifier.
+
+        let drfs: Vec<_> = drf_list
+            .iter()
+            .map(|v| strip_event(&v))
+            .map(add_periodic(update_delay))
+            .collect();
+
         let r = x_min.unwrap_or(0)..(x_max.map(|v| v + 1).unwrap_or(N));
-        let step = window_size
-            .filter(|v| *v > 0 && *v <= r.len())
-            .map(|v| (r.len() + v - 1) / v)
-            .unwrap_or(1);
-        let r = r.step_by(step);
+        let mut reply = types::PlotReplyData {
+            plot_id: "demo".into(),
+            data: drfs
+                .iter()
+                .map(|_| types::PlotChannelData {
+                    channel_units: "V".into(),
+                    channel_status: 0,
+                    channel_data: vec![],
+                })
+                .collect(),
+        };
 
-        let reply = drf_list.iter().fold(
-            types::PlotReplyData {
-                plot_id: "demo".into(),
-                data: vec![],
-            },
-            |mut acc, device| {
-                acc.data.push(match device.as_str() {
-                    "API TEST CONST" => types::PlotChannelData {
-                        channel_units: "A".into(),
-                        channel_status: 0,
-                        channel_data: const_data(&mut r.clone(), 5.0),
-                    },
-                    "API TEST RAMP" => types::PlotChannelData {
-                        channel_units: "V".into(),
-                        channel_status: 0,
-                        channel_data: ramp_data(&mut r.clone()),
-                    },
-                    "API TEST PARABOLA" => types::PlotChannelData {
-                        channel_units: "A".into(),
-                        channel_status: 0,
-                        channel_data: parabola_data(&mut r.clone()),
-                    },
-                    "API TEST SINE" => types::PlotChannelData {
-                        channel_units: "V".into(),
-                        channel_status: 0,
-                        channel_data: sine_data(&mut r.clone()),
-                    },
-                    _ => types::PlotChannelData {
-                        channel_units: "".into(),
-                        channel_status: -1,
-                        channel_data: vec![],
-                    },
+        stuff_fake_data(&mut r.clone(), &drf_list, &mut reply.data);
+
+        match self.accelerator_data(ctxt, drfs, None, None).await {
+            Ok(strm) => {
+                let s = strm.filter_map(move |e: global::DataReply| {
+                    reply.data[e.ref_id as usize].channel_data =
+                        to_plot_data(r.len(), &window_size, &e.data).1;
+                    reply.data[e.ref_id as usize].channel_status = 0;
+
+                    if reply.data.iter().all(|e| {
+                        e.channel_status != 0 || !e.channel_data.is_empty()
+                    }) {
+                        let mut temp = types::PlotReplyData {
+                            plot_id: "demo".into(),
+                            data: reply
+                                .data
+                                .iter()
+                                .map(|e| types::PlotChannelData {
+                                    channel_units: e.channel_units.clone(),
+                                    channel_status: e.channel_status,
+                                    channel_data: vec![],
+                                })
+                                .collect(),
+                        };
+
+                        std::mem::swap(&mut temp, &mut reply);
+                        stuff_fake_data(
+                            &mut r.clone(),
+                            &drf_list,
+                            &mut reply.data,
+                        );
+                        future::ready(Some(temp))
+                    } else {
+                        future::ready(None)
+                    }
                 });
-                acc
-            },
-        );
 
-        Box::pin(stream::once(async { reply })) as PlotStream
+                Box::pin(s) as PlotStream
+            }
+            Err(e) => {
+                error!("{:?}", &e);
+                Box::pin(stream::empty()) as PlotStream
+            }
+        }
     }
 }
 
@@ -320,9 +426,34 @@ fn sine_data(r: &mut dyn Iterator<Item = usize>) -> Vec<types::PlotDataPoint> {
 mod test {
     #[test]
     fn test_removing_event() {
-	use super::strip_event;
+        use super::strip_event;
 
-	assert_eq!(strip_event("abc"), "abc");
-	assert_eq!(strip_event("abc@e,23"), "abc");
+        assert_eq!(strip_event("abc"), "abc");
+        assert_eq!(strip_event("abc@e,23"), "abc");
+
+        assert_eq!(strip_event(""), "");
+        assert_eq!(strip_event("@"), "");
+    }
+
+    #[test]
+    fn test_add_periodic() {
+        use super::add_periodic;
+        use super::{
+            CONST_WAVEFORM, NULL_WAVEFORM, PARABOLA_WAVEFORM, RAMP_WAVEFORM,
+            SINE_WAVEFORM,
+        };
+
+        assert_eq!(add_periodic(None)(CONST_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(None)(RAMP_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(None)(PARABOLA_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(None)(SINE_WAVEFORM), NULL_WAVEFORM);
+
+        assert_eq!(add_periodic(Some(1234))(CONST_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(Some(1234))(RAMP_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(Some(1234))(PARABOLA_WAVEFORM), NULL_WAVEFORM);
+        assert_eq!(add_periodic(Some(1234))(SINE_WAVEFORM), NULL_WAVEFORM);
+
+        assert_eq!(add_periodic(None)("M:OUTTMP"), "M:OUTTMP@p,1000");
+        assert_eq!(add_periodic(Some(1234))("M:OUTTMP"), "M:OUTTMP@p,1234");
     }
 }
