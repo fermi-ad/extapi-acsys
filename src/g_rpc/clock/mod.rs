e51b20a1994aebaff67c7251a3be158a48e83702
Rich Neswold <neswold@fnal.gov>
2025-06-03 14:53:09 -0500

    :art: format code
    Strange. I could have sworn I formatted before committing.

--- src/g_rpc/clock/mod.rs
+++ src/g_rpc/clock/mod.rs
@@ -1,11 +1,10 @@
 use crate::g_rpc::proto::services::aclk::{
-    clock_event_client::ClockEventClient, SubscribeReq, EventInfo
+    clock_event_client::ClockEventClient, EventInfo, SubscribeReq,
 };
 
 pub async fn subscribe(
     events: &[i32],
-) -> Result<tonic::Response<tonic::Streaming<EventInfo>>, tonic::Status>
-{
+) -> Result<tonic::Response<tonic::Streaming<EventInfo>>, tonic::Status> {
     match ClockEventClient::connect("http://clx76.fnal.gov:6803/").await {
         Ok(mut client) => {
             let req = SubscribeReq {
