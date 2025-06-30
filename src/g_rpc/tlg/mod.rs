2d32c875183a4f93816d51e185fb851121522dd9
Rich Neswold <neswold@fnal.gov>
2025-06-30 16:07:40 -0500

    :bug: doesn't need the google module

--- src/g_rpc/tlg/mod.rs
+++ src/g_rpc/tlg/mod.rs
@@ -6,11 +6,6 @@ use proto::services::tlg_placement::{
 use tonic::{transport, Status};
 
 pub mod proto {
-    pub mod google {
-        pub mod r#type {
-            include!("../generated/google.r#type.rs");
-        }
-    }
     pub mod services {
         pub mod tlg_placement {
             include!("../generated/services.tlg_placement.rs");
