b627d94d650747ac853b14a1c99fce4c0ab697b1
Rich Neswold <neswold@fnal.gov>
2025-10-30 15:32:25 -0500

    :art: reformat code

--- build.rs
+++ build.rs
@@ -23,13 +23,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_client(true)
         .build_server(false)
-	.emit_rerun_if_changed(true)
+        .emit_rerun_if_changed(true)
         .compile_protos(&["src/g_rpc/xform/XForm.proto"], &incl)?;
 
     tonic_prost_build::configure()
         .build_client(true)
         .build_server(false)
-	.emit_rerun_if_changed(true)
+        .emit_rerun_if_changed(true)
         .compile_protos(&["src/g_rpc/wscan/WScan.proto"], &incl)?;
 
     tonic_prost_build::configure()
