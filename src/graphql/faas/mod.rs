b304ef3aa9c41fb1346b2ddc1fc5d3e2bb983587
Renee <radkins@fnal.gov>
2025-12-03 15:56:00 -0600

    first pass

--- src/graphql/faas/mod.rs
+++ src/graphql/faas/mod.rs
@@ -6,7 +6,6 @@ use reqwest;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use tracing::instrument;
-use tracing::{info, instrument};
 
 const CLINK_OFFSET: u64 = (24 * 365 * 2 + 6) * 60 * 60;
 
