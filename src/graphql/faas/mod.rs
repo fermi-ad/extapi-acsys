e93a06235676a13c2ded6b4eda2d4d5b2598c220
Renee <radkins@fnal.gov>
2025-12-04 11:29:31 -0600

    call rest api for clinks

--- src/graphql/faas/mod.rs
+++ src/graphql/faas/mod.rs
@@ -1,4 +1,7 @@
+use crate::info;
 use async_graphql::*;
+use reqwest;
+use std::collections::HashMap;
 use tracing::instrument;
 
 const CLINK_OFFSET: u64 = (24 * 365 * 2 + 6) * 60 * 60;
@@ -18,7 +21,18 @@ impl FaasQueries {
     #[graphql(deprecation = "This is a test API and will be removed.")]
     #[instrument(skip(self))]
     async fn clinks_to_unix(&self, clinks: u64) -> u64 {
-        clinks + CLINK_OFFSET
+        info!("[ClinkToUnix] Processing Clinks: {clinks}");
+        let result = reqwest::get(format!(
+            "https://ad-services.fnal.gov/faas/clinks/{}",
+            clinks
+        ))
+        .await
+        .unwrap()
+        .json::<HashMap<String, String>>()
+        .await
+        .unwrap();
+
+        result["unix"].parse().unwrap()
     }
 
     #[doc = "Converts a Unix timestamp (seconds since Jan 1, 1970 UTC) into \
@@ -27,9 +41,14 @@ impl FaasQueries {
 	     conversion fails."]
     #[graphql(deprecation = "This is a test API and will be removed.")]
     #[instrument(skip(self))]
-    async fn unix_to_clinks(&self, time: u64) -> Option<u64> {
-        Some(time)
-            .filter(|v| *v >= CLINK_OFFSET)
-            .map(|v| v - CLINK_OFFSET)
+    async fn unix_to_clinks(&self, time: u64) -> Option<String> {
+        let result =
+            reqwest::get("https://ad-services.fnal.gov/faas/fun-hello-py")
+                .await
+                .ok()?
+                .text()
+                .await
+                .unwrap();
+        Some(result)
     }
 }
