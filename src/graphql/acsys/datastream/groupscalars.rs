879f92bf607813e04c88868c33a41770d9db6110
Rich Neswold <neswold@fnal.gov>
2026-01-14 10:55:42 -0600

    :sparkles: add a stream that accumulates scalar readings
    The EPICS archiver sends one point at a time which is too slow for the
    plotting app (the plotting app does a lot of work each time it gets a
    packet of data.) This stream combinator will be used to buffer up a
    large number of points to amortize the cost of updating the graph.

---
+++ src/graphql/acsys/datastream/groupscalars.rs
@@ -0,0 +1,158 @@
+// Defines a stream that accumulates incoming scalar readings and
+// emits them in a chunk. If the source stream closes, then the
+// pending readings are sent. If a waveform comes in, each waveform is
+// forwarded one at a time. The first element returned by the stream
+// determines what data is forwarded.
+
+use super::{global, DataStream};
+use futures::Stream;
+use futures_util::StreamExt;
+use std::{pin::Pin, task::Poll};
+use tracing::warn;
+
+const MAX_PAYLOAD: usize = 500;
+
+enum StreamState {
+    Unknown,
+    Scalar(global::DataReply),
+    Waveform,
+}
+
+pub struct GroupScalars {
+    archived: DataStream,
+    state: StreamState,
+}
+
+impl GroupScalars {
+    pub fn new(archived: DataStream) -> Self {
+        GroupScalars {
+            archived,
+            state: StreamState::Unknown,
+        }
+    }
+}
+
+pub fn group_scalars(s: DataStream) -> DataStream {
+    Box::pin(GroupScalars::new(s)) as DataStream
+}
+
+impl Stream for GroupScalars {
+    type Item = global::DataReply;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>, ctxt: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        loop {
+            match self.archived.poll_next_unpin(ctxt) {
+                // The stream returned data. Handle the packet
+                // appropriately, based upon the determined stream
+                // type.
+                Poll::Ready(Some(mut payload)) => match &mut self.state {
+                    // The stream type hasn't been determined. Look at
+                    // the first element in the data array to
+                    // determine the stream type.
+                    StreamState::Unknown => match payload.data.as_slice() {
+                        // If the stream has scalar data, set the type
+                        // to "scalar".
+                        &[global::DataInfo {
+                            result: global::DataType::Scalar(_),
+                            ..
+                        }, ..] => {
+                            if payload.data.len() >= MAX_PAYLOAD {
+                                let mut tmp = global::DataReply {
+                                    ref_id: payload.ref_id,
+                                    data: Vec::with_capacity(MAX_PAYLOAD),
+                                };
+
+                                tmp.data
+                                    .extend(payload.data.drain(..MAX_PAYLOAD));
+                                self.state = StreamState::Scalar(payload);
+                                break Poll::Ready(Some(tmp));
+                            } else {
+                                self.state = StreamState::Scalar(payload);
+                            }
+                        }
+
+                        // If the stream has waveform data, set the
+                        // type of the stream to "waveform" for future
+                        // data.
+                        &[global::DataInfo {
+                            result: global::DataType::ScalarArray(_),
+                            ..
+                        }, ..] => {
+                            self.state = StreamState::Waveform;
+                            break Poll::Ready(Some(payload));
+                        }
+
+                        // We don't handle other types of data yet.
+                        _ => {
+                            warn!("archive stream contained non-scalar / non-waveform data");
+                            break Poll::Ready(None);
+                        }
+                    },
+
+                    // For waveform streams, each packet is simply
+                    // forwarded.
+                    StreamState::Waveform => {
+                        break Poll::Ready(Some(payload));
+                    }
+
+                    // If this is a Scalar stream, we append the
+                    // incoming data to the pending data until it
+                    // reaches MAX_PAYLOAD. Once it fills, the
+                    // accumulated data is returned.
+                    StreamState::Scalar(ref mut pending) => {
+                        let space =
+                            MAX_PAYLOAD.saturating_sub(pending.data.len());
+                        let amount_to_move =
+                            std::cmp::min(space, payload.data.len());
+
+                        if amount_to_move > 0 {
+                            pending
+                                .data
+                                .extend(payload.data.drain(..amount_to_move));
+                        }
+
+                        // If the payload data isn't empty, it means
+                        // the pending buffer is full and should be
+                        // sent out.
+
+                        if !payload.data.is_empty() {
+                            // Swap `pending` and `payload` so that
+                            // the state contains the extra and the
+                            // local variable (payload) can be sent
+                            // and dropped.
+
+                            std::mem::swap(pending, &mut payload);
+                            break Poll::Ready(Some(payload));
+                        }
+                    }
+                },
+
+                // Stream is done.
+                Poll::Ready(None) => {
+                    // If the stream returns scalar data, return
+                    // any pending data.
+
+                    if let StreamState::Scalar(ref mut pending) =
+                        &mut self.state
+                    {
+                        if !pending.data.is_empty() {
+                            let mut tmp = global::DataReply {
+                                ref_id: pending.ref_id,
+                                data: vec![],
+                            };
+
+                            std::mem::swap(pending, &mut tmp);
+                            break Poll::Ready(Some(tmp));
+                        }
+                    }
+                    break Poll::Ready(None);
+                }
+
+                // Nothing to read. Return Pending.
+                Poll::Pending => break Poll::Pending,
+            }
+        }
+    }
+}
--- src/graphql/acsys/datastream/mod.rs
+++ src/graphql/acsys/datastream/mod.rs
@@ -5,9 +5,11 @@ mod datachannel;
 mod datamerge;
 mod endondate;
 mod filterdupes;
+mod groupscalars;
 
 pub use archivestream::as_archive_stream;
 pub use datachannel::DataChannel;
 pub use datamerge::merge;
 pub use endondate::end_stream_at;
 pub use filterdupes::filter_dupes;
+pub use groupscalars::group_scalars;
