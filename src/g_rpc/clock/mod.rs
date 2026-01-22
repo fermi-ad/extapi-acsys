104adbde2ffc00733df4982155f0867a0f12211b
Rich Neswold <neswold@fnal.gov>
2026-01-22 16:15:28 -0600

    :wrench: add a timeout to gRPC Clock service
    This adds a 2 second timeout for connecting to the clock service. Before
    this, if the clock service wasn't running, it would take a long time
    before the request would time-out.

--- src/g_rpc/clock/mod.rs
+++ src/g_rpc/clock/mod.rs
@@ -1,7 +1,8 @@
+use tokio::time::Duration;
+use tonic::transport::Endpoint;
 use crate::g_rpc::proto::services::aclk::{
     clock_event_client::ClockEventClient, EventInfo, SubscribeReq,
 };
-
 use crate::env_var;
 
 const CLOCK_HOST: &str = "CLOCK_GRPC_HOST";
@@ -10,15 +11,21 @@ const DEFAULT_CLOCK_HOST: &str = "http://clx76.fnal.gov:6803";
 pub async fn subscribe(
     events: &[i32],
 ) -> Result<tonic::Response<tonic::Streaming<EventInfo>>, tonic::Status> {
-    let host = env_var::get(CLOCK_HOST).or(DEFAULT_CLOCK_HOST.to_owned());
-    match ClockEventClient::connect(host).await {
-        Ok(mut client) => {
-            let req = SubscribeReq {
-                events: events.to_vec(),
-            };
+    let host = env_var::get(CLOCK_HOST)
+        .or_else(|| DEFAULT_CLOCK_HOST.to_owned());
+    let endpoint = Endpoint::from_shared(host)
+        .map_err(|e| {
+            tonic::Status::invalid_argument(format!("Invalid host URI: {}", e))
+        })?
+        .connect_timeout(Duration::from_secs(2));
+    let channel = endpoint
+        .connect()
+        .await
+        .map_err(|_| tonic::Status::unavailable("clock service unavailable"))?;
+    let mut client = ClockEventClient::new(channel);
+    let req = SubscribeReq {
+        events: events.to_vec(),
+    };
 
-            client.subscribe(req).await
-        }
-        Err(_) => Err(tonic::Status::unavailable("clock service unavailable")),
-    }
+    client.subscribe(req).await
 }
