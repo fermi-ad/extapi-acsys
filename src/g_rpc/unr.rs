13adc43168e663f242ff12d1ac229eb4e2ce1306
Connor Howington <chowingt@fnal.gov>
2026-09-03 16:07:47 +0000

    Remove UNR gRPC tests

--- src/g_rpc/unr.rs
+++ src/g_rpc/unr.rs
@@ -23,84 +23,6 @@ use tonic::{
     transport::{Channel, Error},
 };
 
-#[cfg_attr(test, mockall::automock)]
-#[async_trait::async_trait]
-trait UnrBaseInfoClient: Send {
-    async fn create(&mut self, base_info: BaseInfo) -> Result<Empty, Status>;
-    async fn read(
-        &mut self, request: BaseRequest,
-    ) -> Result<BaseResponse, Status>;
-    async fn update(&mut self, base_info: BaseInfo) -> Result<Empty, Status>;
-    async fn delete(&mut self, request: BaseRequest) -> Result<Empty, Status>;
-}
-
-struct TonicUnrBaseInfoClient {
-    inner: BaseInfoServiceClient<Channel>,
-}
-
-#[cfg_attr(test, mockall::automock)]
-#[async_trait::async_trait]
-trait UnrRelationshipClient: Send {
-    async fn read(
-        &mut self, request: RelationshipRequest,
-    ) -> Result<RelationshipResponse, Status>;
-    async fn update(
-        &mut self, relationship_info: RelationshipInfo,
-    ) -> Result<Empty, Status>;
-    async fn delete(
-        &mut self, request: RelationshipRequest,
-    ) -> Result<Empty, Status>;
-}
-
-struct TonicUnrRelationshipClient {
-    inner: RelationshipInfoServiceClient<Channel>,
-}
-
-#[async_trait::async_trait]
-impl UnrRelationshipClient for TonicUnrRelationshipClient {
-    async fn read(
-        &mut self, request: RelationshipRequest,
-    ) -> Result<RelationshipResponse, Status> {
-        self.inner.read(request).await.map(Response::into_inner)
-    }
-
-    async fn update(
-        &mut self, relationship_info: RelationshipInfo,
-    ) -> Result<Empty, Status> {
-        self.inner
-            .update(relationship_info)
-            .await
-            .map(Response::into_inner)
-    }
-
-    async fn delete(
-        &mut self, request: RelationshipRequest,
-    ) -> Result<Empty, Status> {
-        self.inner.delete(request).await.map(Response::into_inner)
-    }
-}
-
-#[async_trait::async_trait]
-impl UnrBaseInfoClient for TonicUnrBaseInfoClient {
-    async fn create(&mut self, base_info: BaseInfo) -> Result<Empty, Status> {
-        self.inner.create(base_info).await.map(Response::into_inner)
-    }
-
-    async fn read(
-        &mut self, request: BaseRequest,
-    ) -> Result<BaseResponse, Status> {
-        self.inner.read(request).await.map(Response::into_inner)
-    }
-
-    async fn update(&mut self, base_info: BaseInfo) -> Result<Empty, Status> {
-        self.inner.update(base_info).await.map(Response::into_inner)
-    }
-
-    async fn delete(&mut self, request: BaseRequest) -> Result<Empty, Status> {
-        self.inner.delete(request).await.map(Response::into_inner)
-    }
-}
-
 /// The environment variable name to use when requesting the location of the UNR gRPC service.
 const UNR_GRPC_HOST: &str = "UNR_GRPC_HOST";
 
@@ -109,38 +31,14 @@ const UNR_GRPC_HOST: &str = "UNR_GRPC_HOST";
 static UNR_CLIENT: LazyLock<ConnectionPort<UnrConnectionAdapter>> =
     LazyLock::new(|| ConnectionPort::new(UNR_GRPC_HOST));
 
-async fn create_base_info_with(
-    client: &mut dyn UnrBaseInfoClient, base_info: BaseInfo,
-) -> Result<Empty, Status> {
-    client.create(base_info).await
-}
-
-async fn read_base_info_with(
-    client: &mut dyn UnrBaseInfoClient, device_names: Vec<String>,
-) -> Result<BaseResponse, Status> {
-    client.read(BaseRequest { device_names }).await
-}
-
-async fn update_base_info_with(
-    client: &mut dyn UnrBaseInfoClient, base_info: BaseInfo,
-) -> Result<Empty, Status> {
-    client.update(base_info).await
-}
-
-async fn delete_base_info_with(
-    client: &mut dyn UnrBaseInfoClient, device_names: Vec<String>,
-) -> Result<Empty, Status> {
-    client.delete(BaseRequest { device_names }).await
-}
-
 /// Makes a request to the UNR gRPC service to create a new `BaseInfo` record.
 pub async fn create_base_info(base_info: BaseInfo) -> Result<Empty, Status> {
-    let do_create = |client: UnrConnectionAdapter| async move {
-        let mut base = TonicUnrBaseInfoClient {
-            inner: client.base_info_conn,
-        };
-        create_base_info_with(&mut base, base_info)
+    let do_create = |mut client: UnrConnectionAdapter| async move {
+        client
+            .base_info_conn
+            .create(base_info)
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_create).await
@@ -151,12 +49,12 @@ pub async fn create_base_info(base_info: BaseInfo) -> Result<Empty, Status> {
 pub async fn read_base_info(
     device_names: Vec<String>,
 ) -> Result<BaseResponse, Status> {
-    let do_read = |client: UnrConnectionAdapter| async move {
-        let mut base = TonicUnrBaseInfoClient {
-            inner: client.base_info_conn,
-        };
-        read_base_info_with(&mut base, device_names)
+    let do_read = |mut client: UnrConnectionAdapter| async move {
+        client
+            .base_info_conn
+            .read(BaseRequest { device_names })
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_read).await
@@ -164,12 +62,12 @@ pub async fn read_base_info(
 
 /// Makes a request to the UNR gRPC service to update an existing `BaseInfo` record.
 pub async fn update_base_info(base_info: BaseInfo) -> Result<Empty, Status> {
-    let do_update = |client: UnrConnectionAdapter| async move {
-        let mut base = TonicUnrBaseInfoClient {
-            inner: client.base_info_conn,
-        };
-        update_base_info_with(&mut base, base_info)
+    let do_update = |mut client: UnrConnectionAdapter| async move {
+        client
+            .base_info_conn
+            .update(base_info)
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_update).await
@@ -179,45 +77,27 @@ pub async fn update_base_info(base_info: BaseInfo) -> Result<Empty, Status> {
 pub async fn delete_base_info(
     device_names: Vec<String>,
 ) -> Result<Empty, Status> {
-    let do_delete = |client: UnrConnectionAdapter| async move {
-        let mut base = TonicUnrBaseInfoClient {
-            inner: client.base_info_conn,
-        };
-        delete_base_info_with(&mut base, device_names)
+    let do_delete = |mut client: UnrConnectionAdapter| async move {
+        client
+            .base_info_conn
+            .delete(BaseRequest { device_names })
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_delete).await
-}
-
-async fn read_relationships_with(
-    client: &mut dyn UnrRelationshipClient, parent_name: String,
-) -> Result<RelationshipResponse, Status> {
-    client.read(RelationshipRequest { parent_name }).await
-}
-
-async fn update_relationships_with(
-    client: &mut dyn UnrRelationshipClient, relationship_info: RelationshipInfo,
-) -> Result<Empty, Status> {
-    client.update(relationship_info).await
-}
-
-async fn delete_relationships_with(
-    client: &mut dyn UnrRelationshipClient, parent_name: String,
-) -> Result<Empty, Status> {
-    client.delete(RelationshipRequest { parent_name }).await
 }
 
 /// Makes a request to the UNR gRPC service to get all children associated with a parent.
 pub async fn read_relationships(
     parent_name: String,
 ) -> Result<RelationshipResponse, Status> {
-    let do_read = |client: UnrConnectionAdapter| async move {
-        let mut rel = TonicUnrRelationshipClient {
-            inner: client.relationship_info_conn,
-        };
-        read_relationships_with(&mut rel, parent_name)
+    let do_read = |mut client: UnrConnectionAdapter| async move {
+        client
+            .relationship_info_conn
+            .read(RelationshipRequest { parent_name })
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_read).await
@@ -227,12 +107,12 @@ pub async fn read_relationships(
 pub async fn update_relationships(
     relationship_info: RelationshipInfo,
 ) -> Result<Empty, Status> {
-    let do_update = |client: UnrConnectionAdapter| async move {
-        let mut rel = TonicUnrRelationshipClient {
-            inner: client.relationship_info_conn,
-        };
-        update_relationships_with(&mut rel, relationship_info)
+    let do_update = |mut client: UnrConnectionAdapter| async move {
+        client
+            .relationship_info_conn
+            .update(relationship_info)
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_update).await
@@ -242,12 +122,12 @@ pub async fn update_relationships(
 pub async fn delete_relationships(
     parent_name: String,
 ) -> Result<Empty, Status> {
-    let do_delete = |client: UnrConnectionAdapter| async move {
-        let mut rel = TonicUnrRelationshipClient {
-            inner: client.relationship_info_conn,
-        };
-        delete_relationships_with(&mut rel, parent_name)
+    let do_delete = |mut client: UnrConnectionAdapter| async move {
+        client
+            .relationship_info_conn
+            .delete(RelationshipRequest { parent_name })
             .await
+            .map(Response::into_inner)
             .map(Into::into)
     };
     UNR_CLIENT.run_with_client(do_delete).await
@@ -272,152 +152,5 @@ impl ConnectionAdapter for UnrConnectionAdapter {
             base_info_conn,
             relationship_info_conn,
         })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn create_base_info_with_calls_create_once_with_expected_payload() {
-        let base_info = BaseInfo {
-            device_name: "DEV".to_string(),
-            address: "ADDR".to_string(),
-            r#type: "TYPE".to_string(),
-            protocol: "PROTO".to_string(),
-        };
-
-        let mut mock = MockUnrBaseInfoClient::new();
-        mock.expect_create()
-            .times(1)
-            .withf({
-                let base_info = base_info.clone();
-                move |got| got == &base_info
-            })
-            .returning(|_| Ok(Empty {}));
-
-        create_base_info_with(&mut mock, base_info).await.unwrap();
-    }
-
-    #[tokio::test]
-    async fn read_base_info_with_calls_read_once_with_expected_request() {
-        let device_names = vec!["A".to_string(), "B".to_string()];
-
-        let mut mock = MockUnrBaseInfoClient::new();
-        mock.expect_read()
-            .times(1)
-            .withf({
-                let device_names = device_names.clone();
-                move |req| req.device_names == device_names
-            })
-            .returning(|_| Ok(BaseResponse { base_info: vec![] }));
-
-        read_base_info_with(&mut mock, device_names).await.unwrap();
-    }
-
-    #[tokio::test]
-    async fn update_base_info_with_calls_update_once_with_expected_payload() {
-        let base_info = BaseInfo {
-            device_name: "DEV".to_string(),
-            address: "ADDR".to_string(),
-            r#type: "TYPE".to_string(),
-            protocol: "PROTO".to_string(),
-        };
-
-        let mut mock = MockUnrBaseInfoClient::new();
-        mock.expect_update()
-            .times(1)
-            .withf({
-                let base_info = base_info.clone();
-                move |got| got == &base_info
-            })
-            .returning(|_| Ok(Empty {}));
-
-        update_base_info_with(&mut mock, base_info).await.unwrap();
-    }
-
-    #[tokio::test]
-    async fn delete_base_info_with_calls_delete_once_with_expected_request() {
-        let device_names = vec!["A".to_string(), "B".to_string()];
-
-        let mut mock = MockUnrBaseInfoClient::new();
-        mock.expect_delete()
-            .times(1)
-            .withf({
-                let device_names = device_names.clone();
-                move |req| req.device_names == device_names
-            })
-            .returning(|_| Ok(Empty {}));
-
-        delete_base_info_with(&mut mock, device_names)
-            .await
-            .unwrap();
-    }
-
-    #[tokio::test]
-    async fn read_relationships_with_calls_read_once_with_expected_request() {
-        let parent_name = "P".to_string();
-
-        let mut mock = MockUnrRelationshipClient::new();
-        mock.expect_read()
-            .times(1)
-            .withf({
-                let parent_name = parent_name.clone();
-                move |req| req.parent_name == parent_name
-            })
-            .returning(|_| {
-                Ok(RelationshipResponse {
-                    relationship_info: Some(RelationshipInfo {
-                        parent_name: "P".to_string(),
-                        children_names: vec![],
-                    }),
-                })
-            });
-
-        read_relationships_with(&mut mock, parent_name)
-            .await
-            .unwrap();
-    }
-
-    #[tokio::test]
-    async fn update_relationships_with_calls_update_once_with_expected_payload()
-    {
-        let relationship_info = RelationshipInfo {
-            parent_name: "P".to_string(),
-            children_names: vec!["C1".to_string(), "C2".to_string()],
-        };
-
-        let mut mock = MockUnrRelationshipClient::new();
-        mock.expect_update()
-            .times(1)
-            .withf({
-                let relationship_info = relationship_info.clone();
-                move |got| got == &relationship_info
-            })
-            .returning(|_| Ok(Empty {}));
-
-        update_relationships_with(&mut mock, relationship_info)
-            .await
-            .unwrap();
-    }
-
-    #[tokio::test]
-    async fn delete_relationships_with_calls_delete_once_with_expected_request()
-    {
-        let parent_name = "P".to_string();
-
-        let mut mock = MockUnrRelationshipClient::new();
-        mock.expect_delete()
-            .times(1)
-            .withf({
-                let parent_name = parent_name.clone();
-                move |req| req.parent_name == parent_name
-            })
-            .returning(|_| Ok(Empty {}));
-
-        delete_relationships_with(&mut mock, parent_name)
-            .await
-            .unwrap();
     }
 }
